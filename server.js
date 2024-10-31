43535db52506a06756c9b1eb34c395e697e66515
Promise Okechukwu <okechukwupromise638@gmail.com>
2024-10-31 15:09:43 +0100

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
@@ -17,6 +18,17 @@ app.use('/channels', channelRoutes);
 // app.use('/api/users', userRoutes);
 // app.use('/api/comments', commentRoutes);
 // app.use('/api/likes', likeRoutes);
+
+
+// Use CORS middleware with default settings to allow all origins
+app.use(cors());
+
+// Alternatively, configure options to be more specific, if needed
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: 'GET,POST,PUT,DELETE', // Allow specific methods
+  allowedHeaders: 'Content-Type,Authorization' // Allow specific headers
+}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
