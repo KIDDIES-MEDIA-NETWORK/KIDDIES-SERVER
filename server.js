50166ec8d9fc395a8c9eb64574db0d98e5c3b448
Promise Okechukwu <okechukwupromise638@gmail.com>
2024-10-31 15:20:14 +0100

    add cors

--- server.js
+++ server.js
@@ -7,28 +7,27 @@ const authRoutes = require('./routes/auth.routes');
 // const commentRoutes = require('./routes/commentRoutes');
 // const likeRoutes = require('./routes/likeRoutes');
 const channelRoutes = require('./routes/channel.routes');
+
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/channels', channelRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/api/comments', commentRoutes);
-// app.use('/api/likes', likeRoutes);
-
-
-// Use CORS middleware with default settings to allow all origins
-app.use(cors());
-
-// Alternatively, configure options to be more specific, if needed
+// Use CORS middleware to allow all origins
 app.use(cors({
   origin: '*', // Allow all origins
   methods: 'GET,POST,PUT,DELETE', // Allow specific methods
   allowedHeaders: 'Content-Type,Authorization' // Allow specific headers
 }));
+
+app.use(express.json());
+
+// Define routes after CORS middleware
+app.use('/api/auth', authRoutes);
+app.use('/channels', channelRoutes);
+// app.use('/api/users', userRoutes);
+// app.use('/api/comments', commentRoutes);
+// app.use('/api/likes', likeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
